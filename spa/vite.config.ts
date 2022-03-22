da12d8cdd615c81634497aecefd0a5c649defa79
Brenton Cozby <5513633+BrentonCozby@users.noreply.github.com>
2022-03-22 16:32:18 -0400

    refactor env variable usage

--- spa/vite.config.ts
+++ spa/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { defineConfig } from 'vite'
+const dotenvFlow = require('dotenv-flow')
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  // @ts-expect-error "need to install @types/node"
-  process.env = {
-    // @ts-expect-error "need to install @types/node"
-    ...process.env,
-    ...loadEnv(mode, '..'),
-  }
+export default () => {
+  dotenvFlow.config({
+    path: '../',
+  })
+
+  process.env.VITE_PHOTOS_SERVICE_PORT = process.env.PHOTOS_SERVICE_PORT
+  process.env.VITE_PHOTOS_SERVICE_HOST = process.env.PHOTOS_SERVICE_HOST
+  process.env.VITE_AUTH0_DOMAIN = process.env.AUTH0_DOMAIN
+  process.env.VITE_AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID
+  process.env.VITE_AUTH0_PHOTOS_API_AUD = process.env.AUTH0_PHOTOS_API_AUD
 
   return defineConfig({
     plugins: [
@@ -18,7 +24,14 @@ export default ({ mode }) => {
     server: {
       host: true,
       // @ts-expect-error "need to install @types/node"
-      port: process.env.VITE_SPA_PORT,
+      port: process.env.SPA_PORT,
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@root': path.resolve(__dirname),
+      },
     },
   })
 }
